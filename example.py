801f3d3638dbbc2cfed2b98da3168a28526ecb8f
Ivan Lazarevich <vanya.lzr@gmail.com>
2020-02-04 15:53:25 +0300

    introduce SpikeDistance class in python API

--- example.py
+++ example.py
@@ -13,19 +13,19 @@ import pyspikelib.mpladeq as mpladeq
 mpladeq.beautify_mpl()
 
 
-datapath = Path("../pyspikelib/data/")
-wake_data = spkutil.load_parquet(datapath / "wake.parq")
-sleep_data = spkutil.load_parquet(datapath / "sleep.parq")
+datapath = Path('../pyspikelib/data/')
+wake_data = spkutil.load_parquet(datapath / 'wake.parq')
+sleep_data = spkutil.load_parquet(datapath / 'sleep.parq')
 
 data = {}
 
-data["wake_train"], data["wake_test"] = spkutil.split_by_spikes(wake_data, ratio=0.5)
-data["sleep_train"], data["sleep_test"] = spkutil.split_by_spikes(sleep_data, ratio=0.5)
+data['wake_train'], data['wake_test'] = spkutil.split_by_spikes(wake_data, ratio=0.5)
+data['sleep_train'], data['sleep_test'] = spkutil.split_by_spikes(sleep_data, ratio=0.5)
 
-p.plot(data["wake_train"]["series"][5][:1000])
+p.plot(data['wake_train']['series'][5][:1000])
 mpladeq.prettify()
-p.xlabel("ISI # in the spike train")
-p.ylabel("ISI value, ms")
+p.xlabel('ISI # in the spike train')
+p.ylabel('ISI value, ms')
 
 window_size = 100
 step_size = 100
@@ -41,23 +41,23 @@ for key in data:
     )
 
 
-[(key, crop_data[key]["series"].shape) for key in crop_data]
+[(key, crop_data[key]['series'].shape) for key in crop_data]
 
-samples = 1000
+samples = 500
 indices = np.random.choice(total_samples, samples)
 
 X_train = np.concatenate(
     [
-        crop_data["wake_train"]["series"][indices, :],
-        crop_data["sleep_train"]["series"][indices, :],
+        crop_data['wake_train']['series'][indices, :],
+        crop_data['sleep_train']['series'][indices, :],
     ]
 )
 y_train = np.array([0] * indices.shape[0] + [1] * indices.shape[0])
 
 X_test = np.concatenate(
     [
-        crop_data["wake_test"]["series"][indices, :],
-        crop_data["sleep_test"]["series"][indices, :],
+        crop_data['wake_test']['series'][indices, :],
+        crop_data['sleep_test']['series'][indices, :],
     ]
 )
 y_test = np.array([0] * indices.shape[0] + [1] * indices.shape[0])
@@ -66,14 +66,14 @@ X_train = np.cumsum(X_train, axis=1)
 X_test = np.cumsum(X_test, axis=1)
 
 train_metric = lambda spike_train1, spike_train2: spknn.distance(
-    spike_train1, spike_train2, metric="isi"
+    spike_train1, spike_train2, metric='schreiber'
 )
 
 knn_classifier = KNeighborsClassifier(
-    n_neighbors=5, weights="uniform", metric=train_metric, n_jobs=-1
+    n_neighbors=5, weights='uniform', metric=train_metric, n_jobs=-1
 )
 
 knn_classifier.fit(X_train, y_train)
-knn_classifier.predict(X_test)
 
-accuracy_score(y_test, knn_classifier.predict(X_test))
+test_predictions = knn_classifier.predict(X_test)
+accuracy_score(y_test, test_predictions)
--- spiketraiNN/spike_distance.py
+++ spiketraiNN/spike_distance.py
@@ -2,30 +2,49 @@ import numpy as np
 import spiketraiNN.distance_binding as dst
 
 
-def spike_train_distance(spike_train1, spike_train2, metric='isi', params=None):
-    PARAMETER_FREE_METRICS = [
-        'schreiber',
-        'isi',
-        'spike',
-        'van_rossum',
-        'max_metric',
-        'modulus_metric',
-    ]
+class SpikeDistance:
+    def __init__(self, metric):
+        self.metric = metric
 
-    safety_eps = 1e-1
-    a_parameter, b_parameter = 0, 0
+    def initialize_metric_parameters(self, spike_train1, spike_train2, q):
+        PARAMETER_FREE_METRICS = [
+            'schreiber',
+            'isi',
+            'spike',
+            'van_rossum',
+            'max_metric',
+            'modulus_metric',
+        ]
+        safety_eps = 1e-1
+        a_parameter, b_parameter = 0, 0
+        if self.metric == 'victor_purpura':
+            a_parameter = q
+        elif self.metric in PARAMETER_FREE_METRICS:
+            joint_train = np.concatenate([spike_train1, spike_train2], axis=0)
+            a_parameter = np.min(joint_train) - safety_eps
+            b_parameter = np.max(joint_train) + safety_eps
 
-    if metric == 'victor_purpura':
-        a_parameter = params['q']
-    elif metric in PARAMETER_FREE_METRICS:
-        joint_train = np.concatenate([spike_train1, spike_train2], axis=0)
-        a_parameter = np.min(joint_train) - safety_eps
-        b_parameter = np.max(joint_train) + safety_eps
+        return a_parameter, b_parameter
 
-    distance = np.zeros(1, dtype='float64')
-    dst.initialize_arrays(spike_train1, spike_train2, distance)
-    dst.set_distance_parameters(
-        a=a_parameter, b=b_parameter, n1=spike_train1.shape[0], n2=spike_train2.shape[0]
-    )
-    dst.compute(metric)
-    return distance[0]
+    def compute(self, spike_train1, spike_train2, q=None):
+        distance = np.zeros(1, dtype='float64')
+        a_parameter, b_parameter = self.initialize_metric_parameters(
+            spike_train1, spike_train2, q
+        )
+        dst.initialize_arrays(spike_train1, spike_train2, distance)
+        dst.set_distance_parameters(
+            a=a_parameter,
+            b=b_parameter,
+            n1=spike_train1.shape[0],
+            n2=spike_train2.shape[0],
+        )
+        dst.compute(self.metric)
+        return distance[0]
+
+    def __call__(self, spike_train1, spike_train2, q=None):
+        return self.compute(spike_train1, spike_train2, q=q)
+
+
+def spike_train_distance(spike_train1, spike_train2, metric='isi', q=None):
+    distance = SpikeDistance(metric=metric)
+    return distance(spike_train1, spike_train2, q=q)
